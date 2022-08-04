08c60c2c09d10a2c2ca314d2c46897e8d44b5bdc
a <breno.nunesgalvao@protonmail.ch>
2022-08-04 10:40:40 -0300

    better sequeler env configuration

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ npx sequelize-cli db:seed:all
 curl localhost:3002/api/things
 ```
 
+# development
+if you have a local postgres with different credentials from the default you can edit them using this .env file
+```
+DATABASE_URL="postgresql://postgres:postgres@localhost:5432"
+```
+the format is postgresql://USER:PASSWORD@HOST:PORT
+is recommended you use an admin user in development as sequelize need the permission to create new databases
+but if that is not possible you can pass a database name using this format postgresql://USER:PASSWORD@HOST:PORT/DATABASE
+
+```
+  npm install -g foreman
+  npx foreman start -f Procfile.dev
+```
+
+# test
+todo
+
+
--- app/server/models/thing.js
+++ app/server/models/thing.js
@@ -1,5 +1,23 @@
-models = require("../../../db/models/index")
+'use strict';
+const { Model } = require('sequelize');
 
-const thing = models.Thing
-
-module.exports = thing;
+module.exports = (sequelize, DataTypes) => {
+  class Thing extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Thing.init({
+    name: DataTypes.STRING,
+    content: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Thing',
+  });
+  return Thing;
+};
